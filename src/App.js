c7640d06d2fbe348f711d39de67436759d990fd7
atthakorn-na <atthakorn.na@ku.th>
2024-05-28 16:58:57 +0700

    add tailwind , card  ,meterial ui , dark theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import Landing from './features/landing';
+import { ThemeProvider, useTheme } from '../src/features/ThemeContext';
+
+
 import './App.css';
+
+function AppContent() {
+  const { theme } = useTheme();
+
+  return (
+    <div className={`App ${theme}`}>
+      <Landing />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Fuck <code>Doohee</code> yedped.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Kao ma yed hee
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <AppContent />
+    </ThemeProvider>
   );
 }
 
